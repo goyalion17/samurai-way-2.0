c285094fa195db33faef167785023594afb20d62
goyalion17 <goyalion17@gmail.com>
2019-12-23 18:37:02 +0200

    11. вёрстка, grid

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
-import Technologies from './components/Technologies/Technologies';
+import Nav from './components/Nav/Nav';
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Technologies />
+    <div className='app-wrapper'>
+      <header className='header'>
+        <div>
+          <img className='logo' src='https://i.pinimg.com/originals/84/b1/06/84b1065e798f61aa80b8670a4b6fbb4d.png' />
+        </div>
+      </header>
+
+      <nav className='nav'>
+        <div><a>Profile</a></div>
+        <div><a>Messages</a></div>
+        <div><a>News</a></div>
+        <div><a>Music</a></div>
+        <div><a>Settings</a></div>
+      </nav>
+
+      <div className='content'>
+        <div>
+          <img src='https://klike.net/uploads/posts/2019-03/1551511784_4.jpg' />
+        </div>
+
+        <div>
+          ava+description
+        </div>
+
+        <div>
+          My posts
+          <div>
+            New Post
+          </div>
+        </div>
+
+        <div>
+          post 1
+        </div>
+        <div>
+          post 2
+        </div>
+      </div>
     </div>      
     )
   }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import s from './Header.module.css';
 
 const Header = () => {
   return (
-      <div>
-        ку
+      <div className={s.header} >
+        <img className={s.logo} src='https://i.pinimg.com/originals/84/b1/06/84b1065e798f61aa80b8670a4b6fbb4d.png' />
       </div>
     )
   }
