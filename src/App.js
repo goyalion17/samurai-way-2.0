46b9e1910a02f64db6af573e094c9d1cd77332a4
goyalion17 <goyalion17@gmail.com>
2019-12-23 16:43:57 +0200

    Component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,29 @@ import './App.css';
 
 const App = () => {
   return (
+    <div>
+      <Header />
+      <Techologies />
+    </div>      
+    )
+  }
+  
+  const Header = () => {
+  return (
       <div className='App'>
+        ку
       </div>
     )
   }
 
+  const Techologies = () => {
+    return (
+        <div className='App'>
+          ка
+        </div>
+      )
+    }
+
 export default App;
+
+// Компонента - это функция, которая принимает props и возвращает jsx
