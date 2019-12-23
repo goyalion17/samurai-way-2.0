a2909083dd3188dcd66f8d734da181987b07df12
goyalion17 <goyalion17@gmail.com>
2019-12-23 15:19:06 +0200

    fix

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render () {
+    return (
+      <div>        
+        Моё не первое реакт-приложение)
+      </div>
+    )
+  }
+}
+
+export default App;
