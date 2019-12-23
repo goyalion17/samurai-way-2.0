fb407e499fad24084d3b9983ec537a73e03668b6
goyalion17 <goyalion17@gmail.com>
2019-12-23 21:01:15 +0200

    14. css-modules

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-//import s from './Header.module.css';
+import s from './Header.module.css';
 
 const Header = () => {
   return (
-      <div className='header'>
+      <div className={s.header}>
         <div>
-          <img className='logo' src='https://i.pinimg.com/originals/84/b1/06/84b1065e798f61aa80b8670a4b6fbb4d.png' />
+          <img className={s.logo} src='https://i.pinimg.com/originals/84/b1/06/84b1065e798f61aa80b8670a4b6fbb4d.png' />
         </div>
       </div>
     )
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Profile.module.css';
 
 const Profile = () => {
   return (
-      <div className='content'>
+      <div className={s.content}>
         <div>
           <img src='https://klike.net/uploads/posts/2019-03/1551511784_4.jpg' />
         </div>
@@ -18,11 +19,13 @@ const Profile = () => {
           </div>
         </div>
 
-        <div>
+        <div className={s.posts}>
+        <div className={s.item}>
           post 1
         </div>
-        <div>
+        <div className={s.item}>
           post 2
+        </div>
         </div>
       </div>
     )
