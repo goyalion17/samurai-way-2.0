63b03338ca69a735462a43cc4cfc9ead9d87a1de
goyalion17 <goyalion17@gmail.com>
2019-12-24 16:17:07 +0200

    21. Верстаем страницу Диалогов

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,7 +2,21 @@ import React from "react";
 import s from "./Dialogs.module.css";
 
 const Dialogs = props => {
-  return <div>Dialogs</div>;
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog + " " + s.active}>Иринка</div>
+        <div className={s.dialog}>Вантик</div>
+        <div className={s.dialog}>Димыч</div>
+      </div>
+
+      <div className={s.messages}>
+        <div className={s.message}>Привет</div>
+        <div className={s.message}>Ку</div>
+        <div className={s.message}>Хаюшки</div>
+      </div>
+    </div>
+  );
 };
 
 export default Dialogs;
