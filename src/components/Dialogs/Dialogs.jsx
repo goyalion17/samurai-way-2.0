e8941f1e69a9494817e7ebe77f4150669db6ba69
goyalion17 <goyalion17@gmail.com>
2019-12-24 18:40:37 +0200

    25. метод массива - map

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -16,25 +16,31 @@ const Message = props => {
 };
 
 const Dialogs = props => {
-  let dialogsData = [
+  let dialogs = [
     { id: 1, name: "Иринка" },
     { id: 2, name: "Вантик" },
     { id: 3, name: "Димыч" }
   ];
-  let messagesData = [
+
+  let messages = [
     { id: 1, message: "Привет" },
     { id: 2, message: "Ку" },
     { id: 3, message: "Хаюшки" }
   ];
+
+  let dialogsElement = dialogs.map(d => (
+    <DialogItem name={d.name} id={d.id} />
+  ));
+
+  let messagesElement = messages.map(m => (
+    <Message message={m.message} />
+  ));
+
   return (
     <div className={s.dialogs}>
-      <div className={s.dialogsItems}>
-        <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
-      </div>
+      <div className={s.dialogsItems}>{dialogsElement}</div>
 
-      <div className={s.messages}>
-        <Message message={messagesData[0].message} />
-      </div>
+      <div className={s.messages}>{messagesElement}</div>
     </div>
   );
 };
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,10 +3,14 @@ import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
 const MyPosts = props => {
-  let postData = [
+  let posts = [
     { id: 1, message: "Hi", likesCount: 15 },
-    { id: 2, message: "Hello", likesCount: 45 }
+    { id: 2, message: "Hello", likesCount: 45 },
+    { id: 3, message: "Yo", likesCount: 27 },
+    { id: 4, message: "What's up", likesCount: 9 }
   ];
+
+  let postsElemets = posts.map( p => <Post message={p.message} likesCount={p.likesCount} />)
 
   return (
     <div className={s.postsBlock}>
@@ -22,10 +26,7 @@ const MyPosts = props => {
         </div>
       </div>
 
-      <div className={s.posts}>
-        <Post message={postData[0].message} likesCount={postData[0].likesCount} />
-        <Post message={postData[1].message} likesCount={postData[1].likesCount} />
-      </div>
+      <div className={s.posts}>{postsElemets}</div>
     </div>
   );
 };
