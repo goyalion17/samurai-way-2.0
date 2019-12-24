adcd275f406f85cbae93584b712e8d5ff45b5291
goyalion17 <goyalion17@gmail.com>
2019-12-24 20:35:03 +0200

    27. component vs render, прокидываем props через Route

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,21 +4,11 @@ import s from "./Dialogs.module.css";
 import Message from "./Message/Message";
 
 const Dialogs = props => {
-  let dialogs = [
-    { id: 1, name: "Иринка" },
-    { id: 2, name: "Вантик" },
-    { id: 3, name: "Димыч" }
-  ];
+  
 
-  let messages = [
-    { id: 1, message: "Привет" },
-    { id: 2, message: "Ку" },
-    { id: 3, message: "Хаюшки" }
-  ];
+  let dialogsElement = props.dialogs.map(d => <DialogItem name={d.name} id={d.id} />);
 
-  let dialogsElement = dialogs.map(d => <DialogItem name={d.name} id={d.id} />);
-
-  let messagesElement = messages.map(m => <Message message={m.message} />);
+  let messagesElement = props.messages.map(m => <Message message={m.message} />);
 
   return (
     <div className={s.dialogs}>
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,15 +2,9 @@ import React from "react";
 import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
-const MyPosts = props => {
-  let posts = [
-    { id: 1, message: "Hi", likesCount: 15 },
-    { id: 2, message: "Hello", likesCount: 45 },
-    { id: 3, message: "Yo", likesCount: 27 },
-    { id: 4, message: "What's up", likesCount: 9 }
-  ];
+const MyPosts = props => {  
 
-  let postsElemets = posts.map( p => <Post message={p.message} likesCount={p.likesCount} />)
+  let postsElemets = props.posts.map( p => <Post message={p.message} likesCount={p.likesCount} />)
 
   return (
     <div className={s.postsBlock}>
