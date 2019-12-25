e243d8d519f2865cf4bd27f456adc858932b1164
goyalion17 <goyalion17@gmail.com>
2019-12-25 15:57:47 +0200

    31. onClick, ref, VirtualDOM

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,15 +4,32 @@ import s from "./Dialogs.module.css";
 import Message from "./Message/Message";
 
 const Dialogs = props => {
-  let dialogsElement = props.state.dialogs.map(d => <DialogItem name={d.name} id={d.id} />);
+  let dialogsElement = props.state.dialogs.map(d => (
+    <DialogItem name={d.name} id={d.id} />
+  ));
 
-  let messagesElement = props.state.messages.map(m => <Message message={m.message} />);
+  let messagesElement = props.state.messages.map(m => (
+    <Message message={m.message} />
+  ));
+
+  let newMessageElement = React.createRef();
+
+  let addMessage = () => {
+    let text = newMessageElement.current.value;
+    return alert(text);
+  }
 
   return (
-    <div className={s.dialogs}>
-      <div className={s.dialogsItems}>{dialogsElement}</div>
+    <div>
+      <div className={s.dialogs}>
+        <div className={s.dialogsItems}>{dialogsElement}</div>
 
-      <div className={s.messages}>{messagesElement}</div>
+        <div className={s.messages}>{messagesElement}</div>
+      </div>
+      <div>
+        <textarea ref={newMessageElement}></textarea>
+      </div>
+      <button onClick={addMessage}>Add message</button>
     </div>
   );
 };
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,9 +2,16 @@ import React from "react";
 import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
-const MyPosts = props => {  
+const MyPosts = props => {
+  let postsElemets = props.posts.map(p => (
+    <Post message={p.message} likesCount={p.likesCount} />
+  ));
 
-  let postsElemets = props.posts.map( p => <Post message={p.message} likesCount={p.likesCount} />)
+  let newPostElement = React.createRef();
+
+  const addPost = () => {
+    let text = newPostElement.current.value;
+  };
 
   return (
     <div className={s.postsBlock}>
@@ -12,10 +19,10 @@ const MyPosts = props => {
         <h4>My posts</h4>
         <div>
           <div>
-            <textarea></textarea>
+            <textarea ref={newPostElement}></textarea>
           </div>
           <div>
-            <button>Add post</button>
+            <button onClick={addPost}>Add post</button>
           </div>
         </div>
       </div>
