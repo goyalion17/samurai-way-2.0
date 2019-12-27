6c320686b1bc31113f444a7cc12016fe404fdb55
goyalion17 <goyalion17@gmail.com>
2019-12-27 22:50:43 +0200

    47. Делаем копию состояния в ProfileReducer

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import store from "./redux/redux-store";
 import * as serviceWorker from "./serviceWorker";
 import {Provider} from 'react-redux';
 
-let rerenderEntireTree = state => {
+
   ReactDOM.render(
     <BrowserRouter>
       <Provider store={store}>
@@ -16,14 +16,7 @@ let rerenderEntireTree = state => {
     </BrowserRouter>,
     document.getElementById("root")
   );
-};
 
-rerenderEntireTree(store.getState());
-
-store.subscribe(() => {
-  let state = store.getState();
-  rerenderEntireTree(state);
-});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
